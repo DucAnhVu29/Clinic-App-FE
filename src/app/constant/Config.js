cd6ab1cfab46ca37018b727e8c9feee86af36445
AnhVDSE161410 <Anhvdse161410@fpt.edu.vn>
2023-07-10 11:11:47 +0700

    First commit

---
+++ src/app/constant/Config.js
@@ -0,0 +1,7 @@
+const Config = {
+  // apiEndpoint: 'http://192.168.10.84', // LOCAL please change it!
+  apiEndpoint: 'https://clinic-app-cs8e.onrender.com', // UAT is not running now!!!
+  apiListeningPort: 3050,
+};
+
+export default Config;
