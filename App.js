5269bf266c86131953fdf7abf98d4343460558f3
phamngocthien <phamngocthien.dev@gmail.com>
2023-07-16 21:35:44 +0700

    add DetailScreen

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-import LoginPage from './src/app/page/LoginPage'
-import RegisterPage from './src/app/page/RegisterPage'
-import AfterLoginMain from './src/app/page/AfterLoginMain'
+import LoginPage from "./src/app/page/LoginPage";
+import RegisterPage from "./src/app/page/RegisterPage";
+import AfterLoginMain from "./src/app/page/AfterLoginMain";
+import DetailScreen from "./src/app/page/DetailScreen";
 
 export default function App() {
   const Stack = createStackNavigator();
@@ -15,6 +16,7 @@ export default function App() {
         <Stack.Screen name="LoginPage" component={LoginPage} />
         <Stack.Screen name="RegisterPage" component={RegisterPage} />
         <Stack.Screen name="AfterLoginMain" component={AfterLoginMain} />
+        <Stack.Screen name="DetailScreen" component={DetailScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
